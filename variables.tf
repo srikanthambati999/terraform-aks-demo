1cff0456e555d86cdd75e5274cc7085b6b9f2e56
srikanthambati999 <49298862+srikanthambati999@users.noreply.github.com>
2020-07-13 15:23:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,30 @@
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+variable "resourcename" {
+  default = "k8s-resources"
 }
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+variable "clustername" {
+  default = "kubernetes-aks1"
 }
-output "fqdn" {
-  value = azurerm_kubernetes_cluster.k8s.fqdn
+variable "location" {
+  default = "East US"
+}
+variable "dnspreffix" {
+  default = "kubecluster"
+}
+variable "size" {
+  default = "Standard_D2_v2"
+}
+variable "agentnode" {
+  default = "1"
+}
+variable "subscription_id" {
+  default = "03fb2e1d-f9c7-4b21-a519-804753d58f45"
+}
+variable "client_id" {
+  default = "6e36c4cd-9f74-4120-995c-5bff8728cf04"
+}
+variable "client_secret" {
+  default = "D5bv_QQH6hfniIg-dtY6fP9q9TVAFJLhd."
+}
+variable "tenant_id" {
+  default = "28743320-645e-4840-8154-b4babd41162c"
 }
